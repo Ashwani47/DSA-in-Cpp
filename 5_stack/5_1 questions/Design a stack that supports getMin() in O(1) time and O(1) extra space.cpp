bf9e7f45005dca3febfb690f52e8d3a748783062
Ashwani singh <ashwani25122004@gmail.com>
2026-06-14 17:08:37 +0530

    Design a stack that supports getMin() in O(1) time and O(1) extra space

---
+++ 5_stack/5_1 questions/Design a stack that supports getMin() in O(1) time and O(1) extra space.cpp
@@ -0,0 +1,47 @@
+class SpecialStack {
+    stack<int> s;
+    int mini;
+
+public:
+
+    void push(int data) {
+        if(s.empty()) {
+            s.push(data);
+            mini = data;
+        }
+        else if(data < mini) {
+            s.push(2 * data - mini);
+            mini = data;
+        }
+        else {
+            s.push(data);
+        }
+    }
+
+    void pop() {
+        if(s.empty())
+            return;
+        int curr = s.top();
+        s.pop();
+        if(curr < mini) {
+            mini = 2 * mini - curr;
+        }
+    }
+
+    int top() {
+        if(s.empty())
+            return -1;
+        int curr = s.top();
+
+        if(curr < mini)
+            return mini;
+        return curr;
+    }
+
+    int getMin() {
+        if(s.empty())
+            return -1;
+
+        return mini;
+    }
+};

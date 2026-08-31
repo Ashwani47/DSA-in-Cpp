a46ceffc4bb7af83bfe353ce44eb01a1d2d04d09
Ashwani singh <ashwani25122004@gmail.com>
2026-08-31 13:46:07 +0530

    Diagonal Tree Traversal

---
+++ 7_Tree/7.1_BinaryTree/questions/DiagonalTreeTraversal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> diagonal(Node *root) {
+        vector<int> ans;
+        if(root == nullptr) return ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            Node* temp = q.front();
+            q.pop();
+            while(temp ){
+                ans.push_back(temp -> data);
+                if(temp->left)
+                    q.push(temp->left);
+                temp = temp -> right;
+            }
+        }
+        return ans;
+    }
+};
